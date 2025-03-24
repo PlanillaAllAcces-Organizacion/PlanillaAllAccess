ac79771bb34f0bfea62060828e77a83301032edf
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-03-24 11:22:00 -0600

    Se agrego codigo para porder traer los registros de empleados guardados en EmpleadoController

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/EmpleadoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PlanillaAllAccessGrupo01.AppWebMVC.Models;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly PlanillaDbContext _context;
+
+        public EmpleadoController(PlanillaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var planillaDbContext = _context.Empleados.Include(e => e.JefeInmediato).Include(e => e.PuestoTrabajo).Include(e => e.TipoDeHorario);
+            return View(await planillaDbContext.ToListAsync());
+        }
+
+    } 
+}
