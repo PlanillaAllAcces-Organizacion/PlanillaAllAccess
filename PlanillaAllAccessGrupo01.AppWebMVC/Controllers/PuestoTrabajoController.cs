b3e797e3a9e5d5232c1945a3edc3b93f1d645279
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-03-25 00:27:23 -0600

    Se creo el controlar del la vista index con las funcciones para realizar busqueda.

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/PuestoTrabajoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PlanillaAllAccessGrupo01.AppWebMVC.Models;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class PuestoTrabajoController : Controller
+    {
+        private readonly PlanillaDbContext _context;
+
+        public PuestoTrabajoController(PlanillaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string nombrePuesto, byte estado = 2, int top = 10)
+        {
+            var estados = new Dictionary<byte, string>
+            {
+                  { 1, "Activo" },
+                  { 0, "Inactivo" }
+             };
+
+            ViewBag.Estados = estados;
+
+            var query = _context.PuestoTrabajos.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(nombrePuesto))
+                query = query.Where(p => p.NombrePuesto.Contains(nombrePuesto));
+
+            if (estado != 2) // 2 indica "Todos los estados"
+                query = query.Where(p => p.Estado == estado);
+
+            query = query.Take(top);
+
+            return View(await query.ToListAsync());
+        }
+
+
+    }
+}
