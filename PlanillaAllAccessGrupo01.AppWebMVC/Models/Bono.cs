6fc31d808012f33882ca324ab67c7c395bdb351c
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-03-25 15:28:12 -0300

    Se modifico en el Model Bono con METODO PARA MOSTRAR EN TEXTO LAS OPCIONES BYTE

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/Bono.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/Bono.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlanillaAllAccessGrupo01.AppWebMVC.Models;
 
@@ -38,4 +39,54 @@ public partial class Bono
     public byte Planilla { get; set; }
 
     public virtual ICollection<AsignacionBono> AsignacionBonos { get; set; } = new List<AsignacionBono>();
+
+
+    #region METODO PARA MOSTRAR EN TEXTO LAS OPCIONES BYTE
+    // ESTO ES EXCLUSIVAMENTE PARA MOSTRAR EN EL INDEX
+    [NotMapped]
+    public string EstadoTexto
+    {
+        get
+        {
+            return Estado switch
+            {
+                0 => "No definida",
+                1 => "Activo",
+                2 => "Inactivo",
+                _ => "Desconocido"
+            };
+        }
+    }
+
+    [NotMapped]
+    public string OperacionTexto
+    {
+        get
+        {
+            return Operacion switch
+            {
+                0 => "No definida",
+                1 => "Operación Fija",
+                2 => "Operación No Fija",
+                _ => "Desconocida"
+            };
+        }
+    }
+
+    [NotMapped]
+    public string PlanillaTexto
+    {
+        get
+        {
+            return Planilla switch
+            {
+                0 => "No definida",
+                1 => "Planilla Mensual",
+                2 => "Planilla Quincenal",
+                _ => "Desconocida"
+            };
+        }
+    }
+
+    #endregion
 }
