b2ffe7b5144dcfbd0cf03b22feb36b1ba3ae6baf
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-03-25 09:46:47 -0600

    Se agregaron display name a cada propiedad del modelo PuestoTrabajo.

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/PuestoTrabajo.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/PuestoTrabajo.cs
@@ -12,21 +12,26 @@ public partial class PuestoTrabajo
 
     [Required(ErrorMessage = "El nombre del puesto es obligatorio.")]
     [StringLength(100, ErrorMessage = "El nombre del puesto es demasiado largo.")]
+    [Display(Name = "Nombre del Puesto")]   
     public string NombrePuesto { get; set; } = null!;
 
     [Required(ErrorMessage = "El salario base es obligatorio.")]
     [Range(0, double.MaxValue, ErrorMessage = "El salario base debe ser un valido.")]
+    [Display(Name = "Salario Base")]
     public decimal SalarioBase { get; set; }
 
     [Required(ErrorMessage = "El valor por hora es obligatorio.")]
     [Range(0, double.MaxValue, ErrorMessage = "El valor por hora debe ser un número positivo.")]
+    [Display(Name = "Valor por Hora")]
     public decimal ValorxHora { get; set; }
 
     [Required(ErrorMessage = "El campo es obligatorio")]
     [Range(0, double.MaxValue, ErrorMessage = "El valor extra debe ser un número positivo.")]
+    [Display(Name = "Valor Extra")]
     public decimal? ValorExtra { get; set; }
 
     [Required(ErrorMessage = "El estado es obligatorio.")]
+    [Display(Name = "Estado")]
     public byte Estado { get; set; }
 
 
