77ab53ddfb4ecede30a4d38a9a5f162d1dc4ebc4
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-03-25 16:00:58 -0300

    Se creo la paginacion en la vista Index del modelo Bono

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/Bono.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/Bono.cs
@@ -30,13 +30,16 @@ public partial class Bono
 
     public DateOnly? FechaExpiracion { get; set; }
 
+
     [Required]
     [Range(0, 2, ErrorMessage = "La operación debe ser 0 (No definida), 1 (Operación Fija) o 2 (Operación No Fija)")]
     public byte Operacion { get; set; }
 
+
     [Required]
     [Range(0, 2, ErrorMessage = "La planilla debe ser 0 (No definida), 1 (Planilla Mensual) o 2 (Planilla Quincenal)")]
     public byte Planilla { get; set; }
+
 
     public virtual ICollection<AsignacionBono> AsignacionBonos { get; set; } = new List<AsignacionBono>();
 
