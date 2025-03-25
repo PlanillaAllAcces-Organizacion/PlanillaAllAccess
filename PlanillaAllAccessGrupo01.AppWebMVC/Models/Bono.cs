8bf9794f9c6e18a1a47a4a8123a308d862312f78
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-03-25 15:21:15 -0300

    Edite el Model Bono y le agrege las validaciones de DataAnnotations

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/Bono.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/Bono.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PlanillaAllAccessGrupo01.AppWebMVC.Models;
 
@@ -7,18 +8,33 @@ public partial class Bono
 {
     public int Id { get; set; }
 
+
+    [Required(ErrorMessage = "El nombre del bono es obligatorio")]
+    [StringLength(100, ErrorMessage = "El nombre del bono no puede exceder los 100 caracteres")]
     public string NombreBono { get; set; } = null!;
 
+
+    [Required(ErrorMessage = "El valor del bono es obligatorio")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El valor debe ser mayor a 0")]
     public decimal Valor { get; set; }
 
+
+    [Required]
+    [Range(0, 2, ErrorMessage = "El estado debe ser 0 (No definida), 1 (Activo) o 2 (Inactivo)")]
     public byte? Estado { get; set; }
+
 
     public DateOnly? FechaValidacion { get; set; }
 
+
     public DateOnly? FechaExpiracion { get; set; }
 
+    [Required]
+    [Range(0, 2, ErrorMessage = "La operación debe ser 0 (No definida), 1 (Operación Fija) o 2 (Operación No Fija)")]
     public byte Operacion { get; set; }
 
+    [Required]
+    [Range(0, 2, ErrorMessage = "La planilla debe ser 0 (No definida), 1 (Planilla Mensual) o 2 (Planilla Quincenal)")]
     public byte Planilla { get; set; }
 
     public virtual ICollection<AsignacionBono> AsignacionBonos { get; set; } = new List<AsignacionBono>();
