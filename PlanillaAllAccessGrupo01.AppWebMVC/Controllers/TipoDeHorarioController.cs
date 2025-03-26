2302a1ef7287cde55a276b2821e7d455582d4398
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-03-26 16:49:11 -0600

    Se creo y se modfico el diseño de la vista de Index,  y tambien se creo el TipodeHorarioController con la funcion de buscar

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/TipoDeHorarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlanillaAllAccessGrupo01.AppWebMVC.Models;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class TipoDeHorarioController : Controller
+    {
+        private readonly PlanillaDbContext _context;
+
+        public TipoDeHorarioController(PlanillaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(TipodeHorario tipodeHorario)
+        {
+            var query = _context.TipodeHorarios.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(tipodeHorario.NombreHorario))
+                query = query.Where(s => s.NombreHorario.Contains(tipodeHorario.NombreHorario));
+
+            query = query.OrderByDescending(s => s.Id);
+
+            return View(await query.ToListAsync());
+        }
+    }
+}
