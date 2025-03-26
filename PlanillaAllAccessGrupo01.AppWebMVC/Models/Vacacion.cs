ebee0fda9d9276f8e2d6f1a5b25da3fa1b551336
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-03-26 08:54:49 -0600

    Se realizaro.n mejoras en el modelo de Vacaiones

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/Vacacion.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/Vacacion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace PlanillaAllAccessGrupo01.AppWebMVC.Models;
 
@@ -9,19 +11,35 @@ public partial class Vacacion
 
     public int EmpleadosId { get; set; }
 
+    [DisplayName("Mes de la vacacion")]
+    [Required(ErrorMessage = "Es necesario el mes de la vacacion")]
     public string MesVacaciones { get; set; } = null!;
 
+
+    [DisplayName("Año de la vacacion")]
+    [Required(ErrorMessage = "Es necesario el año de la vacacion")]
     public string AnnoVacacion { get; set; } = null!;
 
+    [DisplayName("Dia de inicio")]
+    [Required(ErrorMessage = "Es necesario el dia de inicio")]
     public DateTime DiaInicio { get; set; }
 
+
+
+    [DisplayName("Dia fin")]
+    [Required(ErrorMessage = "Es necesario el dia de inicio")]
     public DateTime DiaFin { get; set; }
 
+    [DisplayName("Estado")]
     public byte? Estado { get; set; }
 
+    [DisplayName("¿Vacacion pagada?")]
     public byte? VacacionPagada { get; set; }
 
+    [DisplayName("Pago de la vacacion")]
     public decimal? PagoVacaciones { get; set; }
+
+    [DisplayName("Fecha en la que se le pagara")]
 
     public DateTime? FechaPago { get; set; }
 
