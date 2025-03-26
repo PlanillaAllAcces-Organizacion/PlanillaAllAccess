61a34a1832868f2770f6178198594716fd511fb4
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-03-26 08:25:29 -0600

    Se creo el con trolador Vacacion.

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/VacacionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PlanillaAllAccessGrupo01.AppWebMVC.Models;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class VacacionController : Controller
+    {
+        private readonly PlanillaDbContext _context;
+
+        public VacacionController(PlanillaDbContext context)
+        {
+            _context = context;
+        }
+
+       
+    }
+}
