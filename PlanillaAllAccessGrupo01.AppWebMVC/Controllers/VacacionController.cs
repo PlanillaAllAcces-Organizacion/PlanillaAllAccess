d3a4f9de3173d385629254cd1b5dbdaa20414efe
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-03-26 08:56:00 -0600

    Se hizo el idseño de la vista create de Vacaiones y el controlador GET y POST de Crete.

--- PlanillaAllAccessGrupo01.AppWebMVC/Controllers/VacacionController.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/VacacionController.cs
@@ -18,6 +18,45 @@ namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
             _context = context;
         }
 
-       
+        public IActionResult Create(int id)
+        {
+            var empleado = _context.Empleados.Find(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["EmpleadosId"] = new SelectList(_context.Empleados, "Id", "Nombre", id);
+            return View(new Vacacion { EmpleadosId = id });
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EmpleadosId,MesVacaciones,AnnoVacacion,DiaInicio,DiaFin,Estado,VacacionPagada,PagoVacaciones,FechaPago")] Vacacion vacacion)
+        {
+
+            vacacion.Id = 0;
+
+            if (vacacion.VacacionPagada == 1)
+            {
+                if (vacacion.PagoVacaciones == null && vacacion.FechaPago == null)
+                {
+                    ModelState.AddModelError("PagoVacaciones", "El campo Pago de Vacaciones es requerido.");
+                    ModelState.AddModelError("FechaPago", "El campo Fecha de Pago es requerido.");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(vacacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Empleado");
+            }
+            ViewData["EmpleadosId"] = new SelectList(_context.Empleados, "Id", "Nombre", vacacion.EmpleadosId);
+            return View(vacacion);
+        }
+            
     }
 }
