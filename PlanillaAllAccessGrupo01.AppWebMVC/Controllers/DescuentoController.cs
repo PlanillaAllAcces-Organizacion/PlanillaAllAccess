cb1452485c7b6e0f6d32ff35842f73c5c7b58359
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-03-26 21:01:14 -0600

    Creación del DescuentoController

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/DescuentoController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PlanillaAllAccessGrupo01.AppWebMVC.Models;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class DescuentoController : Controller
+    {
+        private readonly PlanillaDbContext _context;
+
+        public DescuentoController(PlanillaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Descuento
+        public async Task<IActionResult> Index(Descuento descuento, int topRegistro = 10)
+        {
+            var query = _context.Descuentos.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(descuento.Nombre))
+                query = query.Where(s => s.Nombre.Contains(descuento.Nombre));
+
+            if (descuento.Planilla > 0)
+                query = query.Where(s => s.Planilla == descuento.Planilla);
+
+            if (descuento.Estado > 0)
+                query = query.Where(s => s.Estado == descuento.Estado);
+
+            if (descuento.Operacion > 0)
+                query = query.Where(s => s.Operacion == descuento.Operacion);
+
+            if (topRegistro > 0)
+                query = query.Take(topRegistro);
+
+            var listaDescuentos = await query.ToListAsync();
+            foreach (var descuentoItem in listaDescuentos)
+            {
+                descuentoItem.FechaValidacion = DateOnly.FromDateTime(DateTime.Now); // Asignar la fecha de validación
+                descuentoItem.FechaExpiracion = DateOnly.FromDateTime(DateTime.Now.AddMonths(1)); // Asignar la fecha de expiración
+            }
+            return View(listaDescuentos);
+        }
+
+        // GET: Descuento/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var descuento = await _context.Descuentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (descuento == null)
+            {
+                return NotFound();
+            }
+
+            return View(descuento);
+        }
+
+        // GET: Descuento/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Descuento/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Valor,Estado,Operacion,Planilla")] Descuento descuento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(descuento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(descuento);
+        }
+
+        // GET: Descuento/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var descuento = await _context.Descuentos.FindAsync(id);
+            if (descuento == null)
+            {
+                return NotFound();
+            }
+            return View(descuento);
+        }
+
+        // POST: Descuento/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Valor,Estado,Operacion,Planilla")] Descuento descuento)
+        {
+            if (id != descuento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(descuento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DescuentoExists(descuento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(descuento);
+        }
+
+        // GET: Descuento/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var descuento = await _context.Descuentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (descuento == null)
+            {
+                return NotFound();
+            }
+
+            return View(descuento);
+        }
+
+        // POST: Descuento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var descuento = await _context.Descuentos.FindAsync(id);
+            if (descuento != null)
+            {
+                _context.Descuentos.Remove(descuento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DescuentoExists(int id)
+        {
+            return _context.Descuentos.Any(e => e.Id == id);
+        }
+    }
+}
