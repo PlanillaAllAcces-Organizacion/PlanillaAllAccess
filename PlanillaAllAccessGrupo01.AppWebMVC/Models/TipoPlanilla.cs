e4534e2686cdbe15691343912f89e379bd673e97
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-03-29 22:26:07 -0300

    Agregue al modelo los DataAnnotations

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/TipoPlanilla.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/TipoPlanilla.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlanillaAllAccessGrupo01.AppWebMVC.Models;
 
 public partial class TipoPlanilla
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre del tipo de planilla es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres.")]
+    [RegularExpression(@"^[^\d]*$", ErrorMessage = "El nombre no puede contener números.")]
+    [Display(Name = "Nombre del Tipo de Planilla")]
     public string NombreTipo { get; set; } = null!;
 
-    public DateTime FechaCreacion { get; set; }
+    [NotMapped]
+    public DateTime FechaCreacion { get; set; }  = DateTime.Now;
 
     public DateTime? FechaModificacion { get; set; }
 
