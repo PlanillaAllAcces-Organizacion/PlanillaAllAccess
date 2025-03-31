9ed6f57e66c8494ce83ff934cfe50a4cad6232d6
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-03-30 19:38:02 -0600

    Coloque la autorización para el acceso al apartado de Gestion de  Planilla

--- PlanillaAllAccessGrupo01.AppWebMVC/Controllers/PlanillaController.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/PlanillaController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,8 @@ using PlanillaAllAccessGrupo01.AppWebMVC.Models;
 
 namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
 {
+    //Autorización para tener acceso a este apartado de Gestión de Planilla
+    [Authorize]
     public class PlanillaController : Controller
     {
         private readonly PlanillaDbContext _context;
