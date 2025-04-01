07d5f9cd63e2ffd03aa0bb100eb72ca4241cbf70
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-04-01 09:53:16 -0300

    Le agregue validaciones y 1 propiedad NotMapped al modelo de AsignacionBono

--- PlanillaAllAccessGrupo01.AppWebMVC/Models/AsignacionBono.cs
+++ PlanillaAllAccessGrupo01.AppWebMVC/Models/AsignacionBono.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PlanillaAllAccessGrupo01.AppWebMVC.Models;
 
@@ -7,11 +9,19 @@ public partial class AsignacionBono
 {
     public int Id { get; set; }
 
+    [ForeignKey("Empleados")]
     public int EmpleadosId { get; set; }
 
+    [Required(ErrorMessage = "Debe seleccionar un bono.")]
+    [ForeignKey("Bonos")]
     public int BonosId { get; set; }
 
+    [Required]
+    [Range(0, 1, ErrorMessage = "El estado debe ser 0 (Inactivo) o 1 (Activo).")]
     public byte Estado { get; set; }
+
+    [NotMapped]
+    public byte Planilla { get; set; }
 
     public virtual Bono Bonos { get; set; } = null!;
 
