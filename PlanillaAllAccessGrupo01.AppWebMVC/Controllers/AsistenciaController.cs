9dd030328691e484952ae055a973f71e0df85dd3
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-04-02 09:00:55 -0600

    Se creo el controllador de asistencia

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/AsistenciaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class AsistenciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
