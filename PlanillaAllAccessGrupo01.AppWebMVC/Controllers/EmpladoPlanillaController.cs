222bc4acb3118944ae3a94d7682c1db5b271ae19
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-04-02 14:59:28 -0600

    Se creo el empleadoPlanilla

---
+++ PlanillaAllAccessGrupo01.AppWebMVC/Controllers/EmpladoPlanillaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlanillaAllAccessGrupo01.AppWebMVC.Controllers
+{
+    public class EmpladoPlanillaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
